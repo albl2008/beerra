af476168e23ac189b4804380286d29d443f3e27d
Alvaro <alvaroblanco10@gmail.com>
2018-09-26 13:44:10 -0300

    eliminacion de ramas

--- Routes/index.js
+++ Routes/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const api = express.Router();
-const userCtrl = require('../Controllers/userCtrl');
-const User = require('../Models/user');
+
 
 
 api.get('/',(req,res)=>{
     res.end("Soy el login")
 });
-api.post('/signup',userCtrl.createUser);
-api.post('/login/:idUser',userCtrl.getUserById);
-api.post('/login',userCtrl.getEmail);
-//api.get('/profile',userCtrl.auth);
 
 
 module.exports = api;
