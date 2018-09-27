a4027881b67e5305df4ca5d20d53cb877a169943
Alvaro <alvaroblanco10@gmail.com>
2018-09-27 08:03:31 -0300

    auth

---
+++ Middelwares/auth.js
@@ -0,0 +1,19 @@
+const services=require('../Services/index');
+
+function isAuth(req,res,next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message:'No tienes acceso'})
+
+    }
+    const token=req.headers.authorization.split(" ")[1];
+    
+   services.decodeToken(token)
+        .then(response =>{
+            req.user=response,
+            next()
+        })
+        .catch(response =>{
+            res.status(response.status)
+        })         
+}
+module.exports=isAuth
--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const api = express.Router();
+const auth=require('../Middelwares/auth');
+const userCtrl=require('../Controllers/auth');
+
 
 
 
 api.get('/',(req,res)=>{
     res.end("Soy el login")
 });
-
+api.get('/private',auth,function(req,res){
+    res.status(200).send({message:'tienes acceso'});
+});
+api.post('/signin',userCtrl.singIn);
+api.post('/signup',userCtrl.singUp);    
 
 module.exports = api;
