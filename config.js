9e4c9be813b60419e528215fcdd89ffd2f266bd1
Alvaro <alvaroblanco10@gmail.com>
2018-11-15 13:23:43 -0300

    make it better

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports ={
     port : process.env.PORT || 3000,
-    db:process.env.MONGODB || 'mongodb://localhost:27017/beer',
+    db:process.env.MONGODB || 'mongodb://localhost:27017/beerza',
     'secret':'afnm4'
    
 }	
